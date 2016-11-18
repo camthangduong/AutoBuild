42d22e0db05e949ded0ceaa9921b2d373007dc5d
Cam Duong <cam.thang.duong@gmail.com>
2016-11-17 23:25:25 -0500

    Create Build Project

---
+++ MakeBuild/BuildLibrary/IOLibrary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildLibrary
+{
+    class IOLibrary
+    {
+        /// <summary>
+        /// Find the file by its extension from the folder path provided
+        /// </summary>
+        /// <param name="folderPath"></param>
+        /// <param name="folderPath"></param>
+        /// <returns></returns>
+        public string FindTheFileByExtension (string folderPath, string ext)
+        {
+            string returnFileName = "";
+            return returnFileName;
+        }
+
+    }
+}
---
+++ MakeBuild/MakeBuild/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace MakeBuild
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            ///
+            /// Get the current path where the application launched
+            /// Our template re-defined path is C:\GIT
+            /// In order to builds the audit patch, the folder named BUILDS is required where the source folder is            
+            /// 
+            string appPath = Application.ExecutablePath;
+            string appFolder = Path.GetDirectoryName(appPath);
+
+            // Create a BUILDS Folder if it does not exist
+            string buildFolder = Path.Combine(appPath, "BUILDS");
+            if (Directory.Exists(buildFolder) == false)
+            {
+                // Folder does not exist, create a folder
+                Directory.CreateDirectory(buildFolder);
+            }
+
+        }
+    }
+}
